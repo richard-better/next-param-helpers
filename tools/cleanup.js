36143804d97033a41cbaef5ed47214bbfc1ba43c
Richard Borcsik <git@richardb.me>
2022-10-09 17:09:06 +0200

    chore: remove umd and cjs builds, esm as default

--- tools/cleanup.js
+++ tools/cleanup.js
@@ -17,13 +17,4 @@ const deleteFolderRecursive = (path) => {
   }
 }
 
-const folder = process.argv.slice(2)[0]
-
-if (folder) {
-  deleteFolderRecursive(Path.join(__dirname, '../dist', folder))
-} else {
-  deleteFolderRecursive(Path.join(__dirname, '../dist/cjs'))
-  deleteFolderRecursive(Path.join(__dirname, '../dist/esm'))
-  deleteFolderRecursive(Path.join(__dirname, '../dist/umd'))
-  deleteFolderRecursive(Path.join(__dirname, '../dist/types'))
-}
+deleteFolderRecursive(Path.join(__dirname, '../dist'))
