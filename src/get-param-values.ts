a77a052bf31d517e8c45794d9c9269326b782540
Richard Borcsik <git@richardb.me>
2022-10-24 07:58:51 +0200

    feat: use the helpers from next to implement the path matching

--- src/get-param-values.ts
+++ src/get-param-values.ts
@@ -1,12 +1,31 @@
+import { getRouteMatcher } from './helpers/route-matcher'
+import { getRouteRegex } from './helpers/route-regex'
 import type { PageParamObject } from './page-param-object'
 
+/**
+ * Get the values of the parameters of a route
+ * @param route The Next.js route pattern used to match the page
+ * @param url The url to get the parameters from
+ * @returns The parameters of the route
+ * @example
+ * getParamValues('/blog/[id]/[...slug]', '/blog/123/first-post')
+ * // { id: "123", slug: ['first-post'] }
+ * @example
+ * getParamValues('/blog/[id]/[[...slug]]', '/blog/123')
+ * // { id: "123", slug: [] }
+ */
 export const getParamValues = <T extends string>(
-  url: T,
-  params: Record<string, string>
+  route: T,
+  url: string
 ): PageParamObject<T> => {
-  console.log(url)
-  console.log(params)
+  const routeRegex = getRouteRegex(route)
+  const matcher = getRouteMatcher(routeRegex)
 
-  // TODO: implement this function
-  return {} as PageParamObject<T>
+  const match = matcher(url)
+
+  if (!match) {
+    throw new Error(`Could not match "${url}" to the route "${route}"`)
+  }
+
+  return match as PageParamObject<T>
 }
