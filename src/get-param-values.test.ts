64c3b44f0f8b02e038a0150487414a1790cecbb9
Richard Borcsik <git@richardb.me>
2022-10-24 07:59:02 +0200

    test: add a few testcases for the matching

--- src/get-param-values.test.ts
+++ src/get-param-values.test.ts
@@ -1,6 +1,6 @@
 import { getParamValues } from './get-param-values'
 
-test.skip.each<[string, string, Record<string, string | string[]>]>([
+test.each<[string, string, Record<string, string | string[]>]>([
   ['/pattern/[id]', '/pattern/12345', { id: '12345' }],
   ['/pattern/[id]/[[...slug]]', '/pattern/12345', { id: '12345' }],
   [
@@ -8,9 +8,33 @@ test.skip.each<[string, string, Record<string, string | string[]>]>([
     '/pattern/12345/hello/world',
     { id: '12345', slug: ['hello', 'world'] },
   ],
-])('getParamValues returns the correct result', (pattern, url, expected) => {
-  const mockContext = { path: url }
-  const result = getParamValues(pattern, mockContext)
+  [
+    '/a/b/c/d/e/f/g/a/b/c/d/e/f/g/h/i/j/k/l/m/n/o/p/q/i/j/k/l/m/n/o/[p1]/[p2]/[p3]/[p4]/[p5]/[p6]/[p7]/[p8]/[p9]/[p10]/[p11]/[p12]/[p13]/[p14]/[p15]',
+    '/a/b/c/d/e/f/g/a/b/c/d/e/f/g/h/i/j/k/l/m/n/o/p/q/i/j/k/l/m/n/o/1/2/3/4/5/6/7/8/9/10/11/12/13/14/15',
 
-  expect(result).toStrictEqual(expected)
-})
+    {
+      p1: '1',
+      p2: '2',
+      p3: '3',
+      p4: '4',
+      p5: '5',
+      p6: '6',
+      p7: '7',
+      p8: '8',
+      p9: '9',
+      p10: '10',
+      p11: '11',
+      p12: '12',
+      p13: '13',
+      p14: '14',
+      p15: '15',
+    },
+  ],
+])(
+  'getParamValues returns the correct result for %s %s',
+  (pattern, url, expected) => {
+    const result = getParamValues(pattern, url)
+
+    expect(result).toStrictEqual(expected)
+  }
+)
